0cca4a55721f3bd80083652bf33db0f7b2e3b7fe
haikalrozhan <haikalrozhan1992@yahoo.com>
2022-04-14 01:10:10 +0800

    Header and Flatlist

--- App.js
+++ App.js
@@ -1,16 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import CarItem from "./components/CarItem";
+import CarList from "./components/CarList";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <CarItem
-        name="Model X"
-        tagline={"Order Online For"}
-        taglineCTA={"Touchless Delivery"}
-        image={require("./assets/images/ModelX.jpeg")}
-      />
+      <Header />
+      <CarList />
       <StatusBar style="auto" />
     </View>
   );
--- components/CarItem/styles.js
+++ components/CarItem/styles.js
@@ -1,9 +1,9 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Dimensions } from "react-native";
 
 const styles = StyleSheet.create({
   carContainer: {
     width: "100%",
-    height: "100%",
+    height: Dimensions.get("window").height,
   },
   titles: {
     marginTop: "30%",
@@ -19,7 +19,7 @@ const styles = StyleSheet.create({
     color: "#5c5e62",
   },
   subTitleCTA: {
-    textDecorationLine: 'underline'
+    textDecorationLine: "underline",
   },
   image: {
     width: "100%",
